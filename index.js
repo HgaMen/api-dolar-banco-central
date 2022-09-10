a6da2d1497431d6e38515ff24ead326dbda48b21
HgaMen <hga.int2021@gmail.com>
2022-09-10 14:29:45 -0500

    cors public

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import cors from 'cors';
 const app = express();
 
-app.get('/', async (req, res) => {
+// const corsOptions = {
+//   origin: '*',
+// }
+
+app.get('/', cors(), async (req, res) => {
   try {
     const { data } = await axios.get('https://www.bcentral.cl/inicio');
 
