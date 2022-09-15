1c1990be19f0ee2048338f24258379a73049aa6e
HgaMen <hga.int2021@gmail.com>
2022-09-15 17:20:01 -0500

    url not found

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 
 app.use("/api/v1/", coinRouter);
+app.use("/", (_, res) =>
+    res.json({
+        msg: "API pública creada con amos, por favor utilizar con moderación.",
+        version: "1.0.0",
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
