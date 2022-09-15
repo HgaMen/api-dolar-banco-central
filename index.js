6d7dbc59a5d145957855c7c7b6f0d387b1878c66
HgaMen <hga.int2021@gmail.com>
2022-09-15 16:26:16 -0500

    :raised_hands::raised_hands::raised_hands:

--- index.js
+++ index.js
@@ -1,69 +1,12 @@
-import express from 'express';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import coinRouter from "./routes/coin.route.js";
+
 const app = express();
 
-app.get('/', cors(), async (req, res) => {
-  console.log('entró cors');
-  try {
-    // const { data } = await axios.get('https://www.bcentral.cl/inicio');
-    // const dataBitcoin = await axios.get("https://mx.investing.com/crypto/bitcoin/btc-usd")
+app.use(cors());
 
-    const [dataBC, dataBitcoin] = await Promise.allSettled([
-      axios.get('https://www.bcentral.cl/inicio'),
-      axios.get('https://coinmarketcap.com/es/currencies/bitcoin/'),
-    ]);
-
-    // if (dataBC.status === 'rejected') throw error({ msg: dataBC.reason });
-
-    let $ = cheerio.load(dataBC.value.data);
-    const selectorDolar =
-      '#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(3) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2';
-
-    const selectorUF =
-      '#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(1) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2';
-
-    const selectorEuro =
-      '#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(4) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2';
-
-    const objetoDolar = {
-      text: 'dolar',
-      valor: $(selectorDolar).html().split('/')[0].trim() ?? 'sin datos',
-      fecha: new Date().toLocaleDateString(),
-    };
-
-    const objetoUF = {
-      text: 'UF',
-      valor: $(selectorUF).text() ?? 'sin datos', // Nullish coalescing operator / Operador coalescente si el nulo operando izquierdo es un valor falso , no solo nullo undefined
-      fecha: new Date().toLocaleDateString(),
-    };
-
-    const objetoEuro = {
-      text: 'Euro',
-      valor: $(selectorEuro).text() ?? 'sin datos', // Nullish coalescing operator / Operador coalescente si el nulo operando izquierdo es un valor falso , no solo nullo undefined
-      fecha: new Date().toLocaleDateString(),
-    };
-
-    // bitcoin
-
-    $ = cheerio.load(dataBitcoin.value.data);
-    const selectorBitcoin =
-      '#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.eMxKgr.container > div.n78udj-0.jskEGI > div > div.sc-16r8icm-0.kjciSH.priceSection > div.sc-16r8icm-0.kjciSH.priceTitle > div > span';
-
-    // console.log($(selectorBitcoin).text());
-
-    const objetoBitcoin = {
-      text: 'Bitcoin',
-      valor: $(selectorBitcoin).text() ?? 'sin datos', // Nullish coalescing operator / Operador coalescente si el nulo operando izquierdo es un valor falso , no solo nullo undefined
-      fecha: new Date().toLocaleDateString(),
-    };
-
-    return res.json([objetoDolar, objetoUF, objetoEuro, objetoBitcoin]);
-  } catch (error) {
-    res.json({ error });
-  }
-});
+app.use("/api/v1/", coinRouter);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('server en ⭐ puerto: ' + PORT));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
